134b02a99d5964c53879a324feb96834cd37f39d
Akos Seres <seresakos2000@gmail.com>
2021-04-13 20:15:20 +0200

    Add build script for emscripten

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,3 +1,5 @@
 mkdir dest obj
 emcc -c src/main.cpp -o obj/main.o -I include -s USE_SDL=2
-emcc -O3 obj/main.o -o dest/index.html --shell-file index.html -s USE_SDL=2 -s FORCE_FILESYSTEM=1 --preload-file base_scene.obj
+emcc -c src/Shaders.cpp -o obj/Shaders.o -I include -s USE_SDL=2
+emcc -c src/SDLInstance.cpp -o obj/SDLInstance.o -I include -s USE_SDL=2
+emcc obj/main.o obj/Shaders.o obj/SDLInstance.o -o dest/index.html --shell-file index.html -s USE_SDL=2 -s FORCE_FILESYSTEM=1 --preload-file base_scene.obj
