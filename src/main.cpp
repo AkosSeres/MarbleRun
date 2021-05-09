a9121de6d765314625385cca4677f7967d8a51ae
Akos Seres <seresakos2000@gmail.com>
2021-05-09 13:16:47 +0200

    Make main function compatible

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "Scene3D.h"
 
-int main() {
+int main(int argc, char *argv[]) {
   // Create app instance
   Scene3D app = Scene3D("3D Physics sandbox");
   // Enter main loop
   app.enterLoop();
+
+  return 0;
 }
