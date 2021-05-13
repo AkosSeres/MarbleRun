646722c4e26cca6a63d29efd1abc8a08b3158264
Ákos Seres <seresakos2000@gmail.com>
2021-05-13 15:25:16 +0200

    Turn on VSync

--- src/Scene3D.cpp
+++ src/Scene3D.cpp
@@ -92,7 +92,7 @@ void Scene3D::initWindow(char const* titleStr, int w, int h) {
   // presented by opening a browser on virtually any device
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-  SDL_GL_SetSwapInterval(0);
+  SDL_GL_SetSwapInterval(1);
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
